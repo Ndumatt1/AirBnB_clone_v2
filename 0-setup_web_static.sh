d03057278f0f7a60a08230db37479428f2b69fcb
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-04-06 21:56:55 +0100

    upload README and 0-setup_web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# This bash script sets up a webserver for deployment of web_static
+sudo apt-get install -y nginx
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo 'Testing' | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
