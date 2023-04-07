9fe2f5ad5327433f83e87f38d2e7859781a42e99
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-04-07 03:03:57 +0100

    upload 1-pack_web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' This module compresses files on web_static folder to .tgz archive'''
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    ''' generates .tgz archive '''
+    mydate = datetime.now()
+    my_date2 = mydate.strftime("%Y%m%d%H%M%S")
+    path = "versions/web_static_{}.tgz".format(my_date2)
+    local("tar -cvzf {} web_static".format(path))
+    if os.path.exists(path):
+        return path
+    return None
