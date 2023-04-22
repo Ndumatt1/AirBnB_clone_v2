f9f65d1b06a83218e907b26666f386d92f778108
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-04-22 21:11:40 +0100

    upload templates folder and flask files

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python3
-''' This module starts a flask web application'''
+''' THis module starts a flask web application '''
 from flask import Flask
 
-
 app = Flask(__name__)
+app.url_map.strict_slashed = False
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def hello():
-    ''' Displays Hello HBNB'''
+    ''' Displays Hello HBNB!'''
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
     ''' Displays HBNB'''
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>")
 def display_c(text):
-    ''' Displays C followed by the value of the text variable'''
+    ''' Display C followed by the value of the text variable '''
     new_text = text.replace("_", " ")
     return "C {}".format(new_text)
 
+
 @app.route("/python/")
-@app.route("/python/<text>", strict_slashes=False)
-def display_python(text="is cool"):
+@app.route("/python/<text>")
+def display_python(text):
     ''' Displays Python followed by the value of the text variable'''
     new_text = text.replace("_", " ")
     return "Python {}".format(new_text)
+
+
+@app.route("/number/<int:n>")
+def display_number(n):
+    ''' Display n is a number only if n is an integer'''
+    return "{} is a number".format(n)
 
 
 if __name__ == '__main__':
