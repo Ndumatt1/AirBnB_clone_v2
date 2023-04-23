4af0ab15c00e259a700faa69a968db13e44ac64c
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-04-23 16:46:04 +0100

    upload 7-states_list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' This module starts a flask web application'''
+from flask import Flask
+from models import storage
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/states_list")
+def states_list():
+    ''' Displays a HTML page'''
+    states = storage.all()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown():
+    ''' remove the current SQLALchemy Session'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
